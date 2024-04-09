6d3ae6e3c2bb133d58c7b58670f481dc7497b371
lulu <lulubsfu@gmail.com>
2024-04-08 22:46:59 -0400

    server updated

--- server/server.js
+++ server/server.js
@@ -11,14 +11,30 @@ app.use(cors({
 // We use express to define our various API endpoints and
 // provide their handlers that we implemented in routes.js
 app.get('/author/:type', routes.author);
-app.get('/random', routes.random);
-app.get('/song/:song_id', routes.song);
-app.get('/album/:album_id', routes.album);
-app.get('/albums', routes.albums);
-app.get('/album_songs/:album_id', routes.album_songs);
-app.get('/top_songs', routes.top_songs);
-app.get('/top_albums', routes.top_albums);
-app.get('/search_songs', routes.search_songs);
+// app.get('/random', routes.random);
+
+//home page: 
+app.get('/top_courses', routes.top_courses);
+app.get('/top_jobs', routes.top_jobs);
+// app.get('/job_related_courses/:album_id', routes.album_songs);
+
+//glassdoor jobs page:
+app.get('/search_jobs', routes.search_jobs);
+
+//course page: 
+app.get('/search_courses', routes.search_courses);
+
+// job details:
+app.get('/job/:job_id', routes.job);
+
+//course details:
+app.get('/course/:course_id', routes.course);
+
+// app.get('/jobs', routes.albums);
+
+
+
+
 
 app.listen(config.server_port, () => {
   console.log(`Server running at http://${config.server_host}:${config.server_port}/`)
